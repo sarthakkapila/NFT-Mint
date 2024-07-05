996b6d470531d72c47646df478db35413849f661
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-06 02:14:35 +0530

    init harhat

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  console.log("Deploying Contract...");
+  const horoscope = await hre.ethers.deployContract(
+    "contracts/horoscopeNFT.sol:horoscopeNFT"
+  );
+  console.log("Contract deployed to address:", horoscope.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
