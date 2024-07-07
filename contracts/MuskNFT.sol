2e33aad49043afc6acdeb3413b295db1809b3540
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-07 05:33:50 +0530

    added mrmusk contract's structure

--- contracts/MuskNFT.sol
+++ contracts/MuskNFT.sol
@@ -11,17 +11,29 @@ import "hardhat/console.sol";
 contract MuskNFT {
   address public minter;
   address public ElonMusk=0x9a96B40ffE1Fbb5734AbE92400f256b3DBaDa202;   // Using the address of my Phantom wallet here
-  address public PlantTrees=0xF6490d8da3f6Fe027018A01F61eF71f8D69519E4;   // Using the address of my MetaMask wallet here
   uint public MuskPercent = 5;
-  uint public TreesPercent = 5;
 
-  constructor() (string memory _name, string memory _symbol, address _minter, address _ElonMusk, address _PlantTrees, uint _MuskPercent, uint _TreesPercent) ERC721(_name, _symbol) {
+  string[] public quotes;
+
+  constructor() (string memory _name, string memory _symbol, address _minter, address _elonmusk, address _planttrees, uint _muskpercent, uint _treespercent) erc721(_name, _symbol) {
     minter = _minter;
-    ElonMusk = _ElonMusk;
-    PlantTrees = _PlantTrees;
-    MuskPercent = _MuskPercent;
-    TreesPercent = _TreesPercent;
+    elonmusk = _elonmusk;
+    muskpercent = _muskpercent;
+    quotes.push("When something is important enough, you do it even if the odds are not in your favor.");
+    quotes.push("Persistence is very important. You should not give up unless you are forced to give up.");
+    quotes.push("I think it is possible for ordinary people to choose to be extraordinary.");
+    quotes.push("The first step is to establish that something is possible; then probability will occur.");
+    quotes.push("People should pursue what they’re passionate about. That will make them happier than pretty much anything else.");  
   }
+  
+  string basesvg = "<svg xmlns='<http://www.w3.org/2000/svg>' preserveaspectratio='xminymin meet' viewbox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
+
+ function MintNFT(address minter, uint Choice)
+  public 
+  returns(uint256)
+  {
+
+  }
 }
 
